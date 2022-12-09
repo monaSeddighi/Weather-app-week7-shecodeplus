0750272cd76bd04dc4efddaf854e714c1d4558f4
Mona <113529781+monaSeddighi@users.noreply.github.com>
2022-12-09 13:47:12 +0330

    adding description, temperature, city to JS

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
+
+function displayTemperature(response){
+ let temperatureElement=document.querySelector("#temperature")
+temperatureElement.innerHTML=Math.round(response.data.main.temp);
+let cityElement=document.querySelector("#city");
+cityElement.innerHTML=response.data.name;
+let weatherdescriptioElement=document.querySelector("#description");
+weatherdescriptioElement.innerHTML=response.data.weather[0].description;
+}
 let apiKey="5b74d10f3ef03caf1ac640b557c288c3";
 let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
-console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature)
