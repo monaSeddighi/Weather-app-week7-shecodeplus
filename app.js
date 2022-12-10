43588c37305cac0041e00226b49aa8c87207fd08
Mona <113529781+monaSeddighi@users.noreply.github.com>
2022-12-10 20:11:11 +0330

    adding city function in js

--- app.js
+++ app.js
@@ -31,8 +31,18 @@ let iconElement=document.querySelector("#icon");
 iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 iconElement.setAttribute("alt", `http://openweathermap.org/img/wn/${response.data.weather[0].description}@2x.png`);
 }
-let apiKey="5b74d10f3ef03caf1ac640b557c288c3";
-let city="New York";
+function search(city){
+    let apiKey="5b74d10f3ef03caf1ac640b557c288c3";
 let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
 axios.get(apiUrl).then(displayTemperature)
+
+}
+function handleSubmit(event){
+event.prevenDefault();
+let cityInputElement = document.querySelector("#city-input");
+search(cityInputElement.value);
+}
+
+search("New York");
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
