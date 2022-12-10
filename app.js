b80de854361ea7750ef16eb9c35283fc54ad9917
Mona <113529781+monaSeddighi@users.noreply.github.com>
2022-12-10 11:16:57 +0330

    added icon for src and descriptoin for alt

--- app.js
+++ app.js
@@ -14,8 +14,7 @@ return `${day} ${hours}:${minutes}`
 }
 
 
-function displayTemperature(response){
-     let temperatureElement=document.querySelector("#temperature")
+function displayTemperature(response){     let temperatureElement=document.querySelector("#temperature")
 temperatureElement.innerHTML=Math.round(response.data.main.temp);
 let humidityElement=document.querySelector("#humidity")
 humidityElement.innerHTML=Math.round(response.data.main.humidity);
@@ -27,9 +26,12 @@ let cityElement=document.querySelector("#city");
 cityElement.innerHTML=response.data.name;
 let weatherdescriptioElement=document.querySelector("#description");
 weatherdescriptioElement.innerHTML=response.data.weather[0].description;
+let iconElement=document.querySelector("#icon");
+iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+iconElement.setAttribute("alt", `http://openweathermap.org/img/wn/${response.data.weather[0].description}@2x.png`);
 }
 let apiKey="5b74d10f3ef03caf1ac640b557c288c3";
-let city="New York";
+let city="barcelona";
 let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature)
